9bd79d5b6e31ba15d92793830ae9ec05101694ec
Timo Vleer <115629806+TimoV2003@users.noreply.github.com>
2023-04-17 16:00:00 +0200

    GPT Moment

--- src/GameBoyEmulator.java
+++ src/GameBoyEmulator.java
@@ -30,7 +30,7 @@ public class GameBoyEmulator extends JFrame {
 
     private void startEmulationLoop() {
         while (true) {
-            // Fetch the opcode from memory
+            fetchDecodeExecuteOpcode();
 
             // Decode and execute the opcode
 
@@ -56,6 +56,57 @@ public class GameBoyEmulator extends JFrame {
             System.out.println("ROM data loaded successfully.");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void fetchDecodeExecuteOpcode() {
+        // Fetch opcode from memory
+        byte opcode = memory[pc];
+
+        // Decode and execute opcode
+        switch (opcode) {
+            case 0x00:
+                // Opcode 0x00: No operation
+                // Do nothing
+                break;
+            case 0x01:
+                // Opcode 0x01: Load 16-bit immediate value into register BC
+                byte immediateLo = memory[pc + 1];
+                byte immediateHi = memory[pc + 2];
+                int immediateValue = ((immediateHi & 0xFF) << 8) | (immediateLo & 0xFF);
+                bc = immediateValue;
+                break;
+            case 0x02:
+                // Opcode 0x02: Load contents of register A into memory address pointed by register BC
+                operand1 = bc;
+                operand2 = a;
+                writeByteToMemory(operand1, operand2);
+                break;
+            case 0x03:
+                // Opcode 0x03: Increment register BC
+                bc = (bc + 1) & 0xFFFF;
+                break;
+            default:
+                // Unsupported opcode
+                System.out.println("Unsupported opcode: " + Integer.toHexString(opcode & 0xFF));
+                break;
+        }
+
+        pc = (pc + getOpcodeSize(opcode)) & 0xFFFF;
+    }
+
+    private void writeByteToMemory(int address, int value) {
+        memory[address] = (byte) value;
+    }
+
+    private int getOpcodeSize(byte opcode) {
+        // Get size of opcode in bytes
+        // In this example, assuming all opcodes are 1 byte or 3 bytes in size
+        if ((opcode & 0xFF) == 0xCB) {
+            // Opcode 0xCB is a prefix opcode, which is followed by another opcode
+            return 2;
+        } else {
+            return 1;
         }
     }
 }
